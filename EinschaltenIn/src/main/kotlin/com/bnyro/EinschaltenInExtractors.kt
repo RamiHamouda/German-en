2013010855800676f39a9ae29708af5c1d7d1254
Rami Hamouda <91195975+RamiHamouda@users.noreply.github.com>
2025-04-10 22:21:34 -0230

    Delete EinschaltenIn directory

--- EinschaltenIn/src/main/kotlin/com/bnyro/EinschaltenInExtractors.kt
+++
@@ -1,8 +0,0 @@
-package com.bnyro
-
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-
-class D000dCom: DoodLaExtractor() {
-    override var name = "D000d"
-    override var mainUrl = "https://d000d.com"
-}
