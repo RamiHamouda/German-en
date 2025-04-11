e6bfb40f9487eb363816f73ccc21a8e79f46edd8
Rami Hamouda <91195975+RamiHamouda@users.noreply.github.com>
2025-04-10 22:21:19 -0230

    Delete Xcine directory

--- Xcine/src/main/kotlin/com/bnyro/Streamcloud.kt
+++
@@ -1,7 +0,0 @@
-package com.bnyro
-
-class Streamcloud : XCineBase() {
-    override var name = "Streamcloud"
-    override var mainUrl = "https://streamcloud.sx"
-    override var mainAPI = "https://api.streamcloud.sx"
-}
--- Xcine/src/main/kotlin/com/bnyro/XcineIO.kt
+++
@@ -1,7 +0,0 @@
-package com.bnyro
-
-class XcineIO : XCineBase() {
-    override var name = "XcineIO"
-    override var mainUrl = "https://www3.xcine.io"
-    override var mainAPI = "https://api.xcine.io"
-}
